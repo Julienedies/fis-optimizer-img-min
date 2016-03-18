253bc7c749c6b9d51fbfa72eac1dec098052b7b3
Julien.zhang <julien@lulutrip.com>
2016-03-18 18:12:39 +0800

    add cli

---
+++ bin/cli.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+/*
+var Imagemin = require('imagemin');
+
+var conf = {
+    interlaced: true,
+    progressive: true,
+    optimizationLevel: 3
+};
+
+var imagemin = new Imagemin()
+    .src('www/img*/
+/*.{gif,jpg,png,svg}')
+    .dest('min')
+    .use(Imagemin.gifsicle({interlaced: conf.interlaced}))
+    .use(Imagemin.jpegtran({progressive: conf.progressive}))
+    .use(Imagemin.optipng({optimizationLevel: conf.optimizationLevel}))
+    .use(Imagemin.svgo({
+        plugins: conf.svgoPlugins || [],
+        multipass: conf.multipass
+    }));
+
+imagemin.run(function(err, files){
+    err && console.log(err);
+});*/
